eb1b7916a4c17ab993d173d078c2b3a65da256b9
XS170202104682 <861315837@qq.com>
2019-05-21 15:15:11 +0800

    shiyan3

---
+++ src/example/mainframe.java
@@ -0,0 +1,8 @@
+package example;
+
+public class mainframe {
+	public static void main(String[] args) {
+		System.out.println("mainframe");
+
+	}
+}
---
+++ src/example/student.java
@@ -0,0 +1,9 @@
+package example;
+
+public class student {
+
+	public static void main(String[] args) {
+		System.out.println("student");
+	}
+
+}
